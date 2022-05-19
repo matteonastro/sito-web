70fff43a47c8a9be2aee5325533a2ceaed8b2317
matteonastro <nastro.matteo@iisgalvanimi.edu.it>
2022-05-19 12:57:18 +0000

    versione4

---
+++ HTML/script.js
@@ -0,0 +1,9 @@
+function agenda(url){
+    document.getElementById("frame1").src=url
+}
+function obbiettivi(url){
+    document.getElementById("frame1").src=url
+}
+function traguardi(url){
+    document.getElementById("frame1").src=url
+}
